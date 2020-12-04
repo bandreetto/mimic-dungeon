0d11692ba38d55ec8023a4cf7d5f157450d4ed55
pedroprado_bexs <pedro.prado@bexsbanco.com.br>
2020-12-04 09:10:18 -0300

    Setting collision layers for mimics

--- mimic/mimic1/mimic.gd
+++ mimic/mimic1/mimic.gd
@@ -1,5 +1,21 @@
 extends KinematicBody2D
 
+var velocity = Vector2()
+export var isMimic = false
+var hitten = false
+export var direction = 1
 
+func _ready():
+	pass
+	
 func _physics_process(delta):
 	$sprite.play('idle')
+	
+	if isMimic and hitten:
+		$sprite.play('attack')
+		if is_on_wall():
+			direction = direction * -1
+			
+		velocity.y += 2 * direction
+		
+		velocity = move_and_slide(velocity, Vector2.ZERO)
