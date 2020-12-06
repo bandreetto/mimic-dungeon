35ea2cb827ba5c6d5c24da37d436b6a13942f68c
pedroprado_bexs <pedro.prado@bexsbanco.com.br>
2020-12-06 04:26:06 -0300

    Add hit effect

--- scenes/mimic/mimic2/mimic2.gd
+++ scenes/mimic/mimic2/mimic2.gd
@@ -19,17 +19,30 @@ func _physics_process(delta):
 func _on_hurtbox_area_entered(area):
 	knockback = area.knockback_vector * 100
 	stats.health -= area.damage
+	hit_effect()
+	
 
 func _on_stats_no_health():
 	$Timer.start()
 	$sprite.queue_free()
 	$CollisionShape2D.queue_free()
 	$hurtbox.queue_free()
+	death_effect()
+
+
+func _on_Timer_timeout():
+	queue_free()
+
+func death_effect():
 	var scene = load("res://scenes/mimic/mimic2/dieEffect/mimic2DieEffect.tscn")
 	var instance = scene.instance()
 	var main = get_tree().current_scene
 	instance.global_position = global_position
 	main.add_child(instance)
-
-func _on_Timer_timeout():
-	queue_free()
+	
+func hit_effect():
+	var scene = load("res://scenes/effects/hit/hitEffect.tscn")
+	var instance = scene.instance()
+	var main = get_tree().current_scene
+	instance.global_position = global_position
+	main.add_child(instance)
