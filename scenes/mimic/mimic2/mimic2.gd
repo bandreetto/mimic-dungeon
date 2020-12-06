91358fd520412e4da8f054be5d7745aee8cf9a6a
pedroprado_bexs <pedro.prado@bexsbanco.com.br>
2020-12-06 05:09:42 -0300

    Add enemy movement

--- scenes/mimic/mimic2/mimic2.gd
+++ scenes/mimic/mimic2/mimic2.gd
@@ -1,7 +1,21 @@
 extends KinematicBody2D
 
+export var ACCELERATION = 200
+export var MAXSPEED = 50
+export var FRICTION = 100
+
+const dieEffect = preload("res://scenes/mimic/mimic2/dieEffect/mimic2DieEffect.tscn")
+const hitEffect = preload("res://scenes/effects/hit/hitEffect.tscn")
 var knockback = Vector2.ZERO
 onready var stats = $stats
+onready var detection = $PlayerDetectionZone
+onready var sprite = $sprite
+
+enum {
+	IDLE, WANDER, CHASE
+}
+var state = IDLE
+var velocity = Vector2.ZERO
 
 func _ready():
 	pass
@@ -9,12 +23,28 @@ func _ready():
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 100*delta)
 	knockback = move_and_slide(knockback)
-#	if is_on_wall():
-#		direction = direction * -1
-#
-#	velocity.y += 2 * direction
-#
-#	velocity = move_and_slide(velocity, Vector2.ZERO)
+
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION*delta)
+			seek_player()
+		WANDER:
+			pass
+		CHASE:
+			var player = detection.player
+			if player != null:
+				var move_direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(move_direction*MAXSPEED, ACCELERATION*delta)
+			else:
+				state = IDLE	
+	
+	if sprite != null:
+		sprite.flip_h = velocity.x < 0
+	velocity = move_and_slide(velocity)
+
+func seek_player():
+	if detection.can_see_player():
+		state = CHASE
 
 func _on_hurtbox_area_entered(area):
 	knockback = area.knockback_vector * 100
@@ -34,15 +64,13 @@ func _on_Timer_timeout():
 	queue_free()
 
 func death_effect():
-	var scene = load("res://scenes/mimic/mimic2/dieEffect/mimic2DieEffect.tscn")
-	var instance = scene.instance()
+	var instance = dieEffect.instance()
 	var main = get_tree().current_scene
 	instance.global_position = global_position
 	main.add_child(instance)
 	
 func hit_effect():
-	var scene = load("res://scenes/effects/hit/hitEffect.tscn")
-	var instance = scene.instance()
+	var instance = hitEffect.instance()
 	var main = get_tree().current_scene
 	instance.global_position = global_position
 	main.add_child(instance)
