741dca45ad10f6997375121a98337ae1fc094961
pedroprado_bexs <pedro.prado@bexsbanco.com.br>
2020-12-09 12:48:20 -0300

    Fix coin collection

--- scenes/objects/coin/Coin.gd
+++ scenes/objects/coin/Coin.gd
@@ -3,6 +3,7 @@ extends Area2D
 onready var sound = $AudioStreamPlayer
 onready var timer = $Timer
 onready var sprite = $AnimatedSprite
+onready var collision = $CollisionShape2D
 
 func _ready():
 	pass
@@ -12,6 +13,7 @@ func _on_Coin_body_entered(body):
 	PlayerStats.collect_coins(1)
 	sound.play()
 	if weakref(sprite).get_ref(): 
+		collision.queue_free()
 		sprite.queue_free()
 	timer.start(1)
 
